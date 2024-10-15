a391521e4302519a148070a457c649d6ec50ed59
tim569 <timothyobrike@gmail.com>
2024-10-15 06:44:59 +0100

    updated main function

--- main.py
+++ main.py
@@ -14,13 +14,11 @@ source_dir = config['SOURCE_DIR']
 json_filename = config['JsonFileName']
 
 c_files = [f for f in os.listdir(source_dir) if f.endswith('.c')]
-cpp_files = [f for f in os.listdir(source_dir) if f.endswith('.cpp')]
+cpp_files = [f for f in os.listdir(source_dir) if (f.endswith('.cpp') or f.endswith('.cxx')) ]
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    readfiles.compile_and_dump(c_command, json_filename, source_dir, output_dir, c_files,)
-    executefiles.execute_and_dump(json_filename, output_dir)
-
-
-
+    #readfiles.compile_and_dump(cpp_command, json_filename, source_dir, output_dir, cpp_files,)
+    #executefiles.execute_and_dump(json_filename, output_dir)
+    readfiles.compile_and_dump_dir('cc', 'c', json_filename, source_dir, output_dir,)
