8301f3e3e26cedc90b5a2ddee0159d0d4b6cfe64
tim569 <timothyobrike@gmail.com>
2024-10-16 14:42:33 +0100

    created a compile function

--- compile.py
+++ compile.py
@@ -5,8 +5,21 @@ import json
 from utils import executefiles
 from utils.readfiles import lazy_load_func
 from utils.logging import log_to_file
+from utils.readfiles import change_attribute
+
+# redundant lol
+from main import cc_command
+
+
 
 class CompileMachine:
+    def lazy_load_impl(self):
+        if self.lazy_load and self._number_of_compiles > 0:
+            self.files = lazy_load_func(self.json_filename)
+            print(f"Lazy load activated for compilation...")
+        else:
+            self.files = self.files_to_compile(self.file_type)
+
     def files_to_compile(self, filetype):
         if filetype == 'c':
             return [f for f in os.listdir(self.source_dir) if f.endswith('.c')]
@@ -14,15 +27,39 @@ class CompileMachine:
         elif filetype == 'c++':
             return [f for f in os.listdir(self.source_dir) if (f.endswith('.cpp') or f.endswith('.cxx'))]
 
+    def compile(self, runcommands, execute, results, file_to_compile, output_bin) -> list:
+        try:
+            subprocess.run(runcommands, capture_output=True, text=True, check=True)
+            print(f"Compilation of {file_to_compile} successful")
+            results['compile_status'] = 'success'
 
-    def __init__(self, command, json_filename, log_file, source_dir, output_dir, file_type, lazy_load=False, compile_dir_to_executable=False):
+            if execute:
+                print(f"Executing {output_bin}...")
+                stdout, stderr = executefiles.execute(platform.system(), output_bin)
+                results['output'] = {"stdout": stdout, "stderr": stderr}
+
+            else:
+                results['output'] = ''
+
+        except subprocess.CalledProcessError as e:
+            print(f"Error during compilation of {file_to_compile}")
+            results['compile_status'] = 'failure'
+
+            # capture the error message; Error message of death!!!
+            results['output'] = {"stdout": e.stdout,
+                                               "stderr": e.stderr}
+
+        finally:
+            return results
+    def __init__(self, command, json_filename, log_file, source_dir, output_dir, file_type, lazy_load=False,
+                 compile_dir_to_executable=False):
         self.files = None
         self.command = command
         self.json_filename = json_filename
         self.source_dir = source_dir
         self.output_dir = output_dir
         self.file_type = file_type
-        self.log_file =log_file
+        self.log_file = log_file
         self._number_of_compiles = 0
         if lazy_load:
             self.lazy_load = True
@@ -34,18 +71,14 @@ class CompileMachine:
         else:
             self.dir_to_executable = False
 
-    def compile_and_dump_each(self, executeFlag=False):
+    def compile_and_dump_exec_each(self, executeFlag=False):
         compilation_results = []
         extension = ''
         if platform.system() == "Windows":
             extension = '.exe'
 
         # check if lazy load is active
-        if self.lazy_load and self._number_of_compiles > 0:
-            self.files = lazy_load_func(self.json_filename)
-            print(f"Lazy load activated for compilation...")
-        else:
-            self.files = self.files_to_compile(self.file_type)
+        self.lazy_load_impl()
 
         # compilation process
         for file in self.files:
@@ -59,26 +92,11 @@ class CompileMachine:
                 'compile_status': '',
                 'output': '',
             }
-            try:
-                subprocess.run(runcommands, capture_output=True, text=True, check=True)
-                print(f"Compilation of {file} successful")
-                result_of_compilation['compile_status'] = 'success'
 
-                if executeFlag:
-                    print(f"Executing {output_binary}...")
-                    stdout, stderr = executefiles.execute(platform.system(), output_binary)
-                    result_of_compilation['output'] = {"stdout": stdout, "stderr": stderr}
+            result_of_compilation = self.compile(runcommands, execute=executeFlag,
+                                                 results=result_of_compilation, file_to_compile=file,
+                                                 output_bin=output_binary)
 
-                else:
-                    result_of_compilation['output'] = ''
-
-            except subprocess.CalledProcessError as e:
-                print(f"Error during compilation of {file}")
-                result_of_compilation['compile_status'] = 'failure'
-
-                # capture the error message; Error message of death!!!
-                result_of_compilation['output'] = {"stdout": e.stdout,
-                                                   "stderr": e.stderr}
             compilation_results.append(result_of_compilation)
 
         json_file = os.path.join('./', self.json_filename)
@@ -89,10 +107,9 @@ class CompileMachine:
         # logging logic
         log_to_file(json_file, self.log_file, self.lazy_load)
 
-
     # TODO: CONFIGURE THIS FUNCTION
 
-    def compile_and_dump_dir(self, executeFlag=False, ExecName='a'):
+    def compile_and_dump_exec_dir(self, executeFlag=False, ExecName='a'):
         compilation_results = []
         extension = ''
         if platform.system() != "Windows":
@@ -100,6 +117,9 @@ class CompileMachine:
                 extension = '.out'
 
         extension = '.exe'  # will evaluate if we are on windows
+
+        # change command to cc
+        self.command = change_attribute(self, 'command', cc_command)
 
         # lang_type is list
         files_to_compile = self.files_to_compile(filetype=self.file_type)
@@ -112,11 +132,15 @@ class CompileMachine:
         runcommands.append(output_binary)
 
         result_of_compilation = {
-            'dir': [self.command, ExecName + extension],
+            'dir': [self.source_dir, ExecName + extension],
             'compile_link_status': '',
             'output': '',
         }
 
+        # check if lazy load is active
+        self.lazy_load_impl()
+
+        # compilation process
         try:
             subprocess.run(runcommands, capture_output=True, text=True, check=True)
             print(f"Compilation of {self.source_dir} successful")
